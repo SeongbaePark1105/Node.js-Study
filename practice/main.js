ec46bf3b49da8760a3dc4ce8be1b2f7ae8bbbe3b
SeongbaePark1105 <sbpark0402@gmail.com>
2022-07-08 20:34:57 +0900

    22-07-08

--- practice/main.js
+++ practice/main.js
@@ -5,8 +5,9 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   console.log(queryData.id); // query 값 받아옴
+  var title = queryData.id;
   if (_url == "/") {
-    _url = "/index.html";
+    title = "Welcome";
   }
   if (_url == "/favicon.ico") {
     response.writeHead(404);
@@ -14,7 +15,27 @@ var app = http.createServer(function (request, response) {
     return;
   }
   response.writeHead(200);
+  fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
+    var template = `<!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScrpit">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
+    `;
+    response.end(template);
+  });
   //response.end(fs.readFileSync(__dirname + _url));
-  response.end(queryData.id);
 });
 app.listen(3000);
