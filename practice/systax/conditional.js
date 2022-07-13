b84b737d0ca90a5b7e2588ca20d0f00155deda92
SeongbaePark1105 <sbpark0402@gmail.com>
2022-07-13 20:45:31 +0900

    22-07-13

--- practice/systax/conditional.js
+++ practice/systax/conditional.js
@@ -1,9 +1,9 @@
+var args = process.argv;
+console.log(args[2]);
 console.log("A");
 console.log("B");
-if (true) {
+if (args[2] == "1") {
   console.log("C1");
-}
-if (false) {
-  console.log("C2");
-}
+} else console.log("C2");
+
 console.log("D");
