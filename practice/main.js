678a8b43c6904de5eed65c6d6580d0867ec19791
박성배 <98007970+SeongbaePark1105@users.noreply.github.com>
2022-07-14 21:46:07 +0900

    22-07-14(main-functoin)

--- practice/main.js
+++ practice/main.js
@@ -1,6 +1,31 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+function templateHTML(title, list, body){
+  return  `<!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = "<ul>";
+  var i = 0;
+  while (i < filelist.length) {
+    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i += 1;
+  }
+  list += "</ul>";
+  return list;
+}
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -14,27 +39,8 @@ var app = http.createServer(function (request, response) {
         console.log(filelist);
         var title = `Welcome`;
         var description = "Hello, Node.js";
-        var list = "<ul>";
-        var i = 0;
-        while (i < filelist.length) {
-          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i += 1;
-        }
-        list += "</ul>";
-        var template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
         response.writeHead(200); // 전송 성공
         response.end(template);
       });
@@ -43,29 +49,10 @@ var app = http.createServer(function (request, response) {
         console.log(filelist);
         var title = `Welcome`;
         var description = "Hello, Node.js";
-        var list = "<ul>";
-        var i = 0;
-        while (i < filelist.length) {
-          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i += 1;
-        }
-        list += "</ul>";
+        var list = templateList(filelist);
         fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
           var title = queryData.id;
-          var template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200); // 전송 성공
           response.end(template);
           //response.end(fs.readFileSync(__dirname + _url));
