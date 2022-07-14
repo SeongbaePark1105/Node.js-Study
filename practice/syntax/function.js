cc982e593ba69bd4bb3c254563b0e8fec92b8a95
박성배 <98007970+SeongbaePark1105@users.noreply.github.com>
2022-07-14 21:09:42 +0900

    22-07-14(Folder Rename)

---
+++ practice/syntax/function.js
@@ -0,0 +1,17 @@
+f123();
+console.log('A');
+console.log('B');
+console.log('C');
+console.log('D');
+f123();
+console.log('E');
+console.log('F');
+console.log('G');
+console.log('H');
+
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+
+}
